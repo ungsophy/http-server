aefa10d76cf81305a5fbf85e12c8a3de2359358d
sophy <sophy@invisionapp.com>
2025-07-19 00:12:37 +0700

    Reuse tcp connection

--- app/main.go
+++ app/main.go
@@ -55,109 +55,120 @@ func main() {
 }
 
 func handleConnection(conn net.Conn) {
-	// defer func() {
-	// 	closeErr := conn.Close()
-	// 	if closeErr != nil {
-	// 		fmt.Println("error closing connection: ", closeErr.Error())
-	// 	}
-	// }()
+	var req *Request
+
+	defer func() {
+		if req.Headers["Connection"] != "close" {
+			return
+		}
+
+		closeErr := conn.Close()
+		if closeErr != nil {
+			fmt.Println("error closing connection: ", closeErr.Error())
+		}
+	}()
 
 	fmt.Println("new connection from", conn.RemoteAddr().String())
 
-	reqBuf := make([]byte, 1024)
-	_, readErr := conn.Read(reqBuf)
-	if readErr != nil {
-		fmt.Println("error reading request: ", readErr.Error())
-		return
-	}
-
-	req, err := ParseRequest(reqBuf)
-	if err != nil {
-		fmt.Println("error parsing request: ", err.Error())
-		return
-	}
-
-	var statusCode int = 404
-	var headers = make(map[string]string)
-	var body []byte
-
-	if req.Path == "/" {
-		statusCode = 200
-	} else if req.Path == "/user-agent" {
-		statusCode = 200
-		headers["Content-Type"] = "text/plain"
-		body = []byte(req.Headers["User-Agent"])
-	} else if strings.Index(req.Path, "/echo/") == 0 {
-		str := strings.Replace(req.Path, "/echo/", "", 1)
-
-		for _, encoding := range req.Encodings() {
-			if encoder, exists := encoders[encoding]; exists {
-				encodedBody, encodeErr := encoder.Encode([]byte(str))
-				if encodeErr != nil {
-					fmt.Println("error encoding response: ", encodeErr.Error())
-					return
-				}
-
-				body = encodedBody
-				headers["Content-Encoding"] = encoding
-				break
+	for {
+		var reqBuf = make([]byte, 1024)
+		_, readErr := conn.Read(reqBuf)
+		if readErr != nil {
+			if !errors.Is(readErr, io.EOF) {
+				fmt.Println("error reading request: ", readErr.Error())
 			}
+			return
 		}
 
-		// If no encoding was applied, use the original body
-		if headers["Content-Encoding"] == "" {
+		var parseReqErr error
+		req, parseReqErr = ParseRequest(reqBuf)
+		if parseReqErr != nil {
+			fmt.Println("error parsing request: ", parseReqErr.Error())
+			return
+		}
+
+		var statusCode int = 404
+		var headers = make(map[string]string)
+		var body []byte
+
+		if req.Path == "/" {
+			statusCode = 200
+		} else if req.Path == "/user-agent" {
+			statusCode = 200
 			headers["Content-Type"] = "text/plain"
-			body = []byte(str)
-		}
+			body = []byte(req.Headers["User-Agent"])
+		} else if strings.Index(req.Path, "/echo/") == 0 {
+			str := strings.Replace(req.Path, "/echo/", "", 1)
 
-		statusCode = 200
-	} else if strings.Index(req.Path, "/files/") == 0 {
-		// Ensure the directory is set
-		_, filename := path.Split(req.Path)
-		filepath := filepath.Join(*directory, filename)
+			for _, encoding := range req.Encodings() {
+				if encoder, exists := encoders[encoding]; exists {
+					encodedBody, encodeErr := encoder.Encode([]byte(str))
+					if encodeErr != nil {
+						fmt.Println("error encoding response: ", encodeErr.Error())
+						return
+					}
 
-		switch req.Method {
-		case "GET":
-			file, openErr := os.Open(filepath)
-			defer file.Close()
+					body = encodedBody
+					headers["Content-Encoding"] = encoding
+					break
+				}
+			}
 
-			if openErr == nil {
-				var readErr error
-				body, readErr = io.ReadAll(file)
-				if readErr != nil {
-					fmt.Println("error reading file: ", readErr.Error())
+			// If no encoding was applied, use the original body
+			if headers["Content-Encoding"] == "" {
+				headers["Content-Type"] = "text/plain"
+				body = []byte(str)
+			}
+
+			statusCode = 200
+		} else if strings.Index(req.Path, "/files/") == 0 {
+			// Ensure the directory is set
+			_, filename := path.Split(req.Path)
+			filepath := filepath.Join(*directory, filename)
+
+			switch req.Method {
+			case "GET":
+				file, openErr := os.Open(filepath)
+				defer file.Close()
+
+				if openErr == nil {
+					var readErr error
+					body, readErr = io.ReadAll(file)
+					if readErr != nil {
+						fmt.Println("error reading file: ", readErr.Error())
+						return
+					}
+
+					statusCode = 200
+					headers["Content-Type"] = "application/octet-stream"
+				} else {
+					fmt.Println("error opening file: ", openErr.Error())
+					statusCode = 404
+				}
+			case "POST":
+				file, createErr := os.Create(filepath)
+				defer file.Close()
+
+				if createErr != nil {
+					fmt.Println("error creating file: ", createErr.Error())
 					return
 				}
 
-				statusCode = 200
-				headers["Content-Type"] = "application/octet-stream"
-			} else {
-				fmt.Println("error opening file: ", openErr.Error())
-				statusCode = 404
-			}
-		case "POST":
-			file, createErr := os.Create(filepath)
-			defer file.Close()
-
-			if createErr != nil {
-				fmt.Println("error creating file: ", createErr.Error())
-				return
-			}
-
-			_, writeErr := file.Write(req.Body)
-			if writeErr != nil {
-				fmt.Println("error writing file: ", writeErr.Error())
-				statusCode = 500
-			} else {
-				statusCode = 201
+				_, writeErr := file.Write(req.Body)
+				if writeErr != nil {
+					fmt.Println("error writing file: ", writeErr.Error())
+					statusCode = 500
+				} else {
+					statusCode = 201
+				}
 			}
 		}
-	}
 
-	resp := &Response{
-		StatusCode: statusCode,
-		Headers:    headers,
-		Body:       body,
+		resp := &Response{
+			StatusCode: statusCode,
+			Headers:    headers,
+			Body:       body,
+		}
+		conn.Write(resp.Bytes())
 	}
-	conn.Write(resp.Bytes())
 }
